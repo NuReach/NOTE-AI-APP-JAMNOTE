ceb7f8126aaeab8f49ec694c65aa813608fd5a50
NuReach <nureach46@gmail.com>
2024-05-02 02:00:10 +0700

    add font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import localFont from "next/font/local";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,6 +10,10 @@ export const metadata: Metadata = {
   title: "JAM NOTE",
   description: "This Interllignet Note Taking App",
 };
+
+const myFont = localFont({
+  src: "/fonts/myFont.ttf",
+});
 
 export default function RootLayout({
   children,
@@ -18,7 +23,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={myFont.className}>{children}</body>
       </html>
     </ClerkProvider>
   );
